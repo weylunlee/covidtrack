da1e7e397284c29d6d251efcad821e3ed264d294
Kevin Lee <weylunlee@gmail.com>
2020-06-20 13:24:21 -0700

    refactored and cleaned up code

--- scripts/chartScript.js
+++ scripts/chartScript.js
@@ -1,8 +1,8 @@
-// Creates series and chart given array of array[date][new_confirmed_cases]
+// Creates json and chart given array of array[date][new_confirmed_cases]
 function calcAveragesAndChart(count, chartDiv, chartName, avgType) {
-    var movingAvg1, movingAvg2;
-    
-    if(avgType == 'MA') {
+    let movingAvg1, movingAvg2;
+
+    if (avgType == 'MA') {
         movingAvg1 = calcMovingAverage(count, 7);
         movingAvg2 = calcMovingAverage(count, 14);
     }
@@ -12,70 +12,57 @@ function calcAveragesAndChart(count, chartDiv, chartName, avgType) {
     }
 
     createChart(
-    [
-        {type: 'column', bar_width: 1, color: '#7ecef9', name: 'New Cases', points: count},
-        {type: 'line spline', line_width: 3, color: '#3b577f', name: '7-Day ' + avgType, points: movingAvg1},
-        {type: 'line spline', line_width: 3, color: '#CF5864', name: '14-Day ' + avgType, points: movingAvg2}
-    ], chartDiv, chartName);
+        [
+            { type: 'column', bar_width: 1, color: '#7ecef9', name: 'New Cases', points: count },
+            { type: 'line spline', line_width: 3, color: '#3b577f', name: '7-Day ' + avgType, points: movingAvg1 },
+            { type: 'line spline', line_width: 3, color: '#CF5864', name: '14-Day ' + avgType, points: movingAvg2 }
+        ], chartDiv, chartName);
 }
 
 // Calculates new confirmed based on daily cumulative confirmed
 function calcNewConfirmed(count) {
     let newConfirmed = [];
-    
-    // Check if ascending or descending order!
-    // Assumes dates are not ordered randomly, and no missing dates!
-    let ascending = true;
+
+    // Check if descending order. Assumes dates are not ordered randomly, and no missing dates!
     if (count.length > 1 && count[0].x > count[1].x) {
-        ascending = false;
+        count.reverse();
     }
-    
-    if (ascending) {
-        // Start with 0
-        newConfirmed.push({x: count[0].x, y: 0});
-        
-        for (var i=1; i<count.length; i++) {
-            // If next day is less than previous day, use previous day
-            newConfirmed.push({x: count[i].x, y: count[i].y < count[i-1].y ? 0 : count[i].y - count[i-1].y});
-        }
+
+    // Start with 0
+    newConfirmed.push({ 
+        x: count[0].x, 
+        y: 0 });
+
+    for (let i = 1; i < count.length; i++) {
+        // If next day is less than previous day, use previous day
+        newConfirmed.push({
+            x: count[i].x,
+            y: count[i].y < count[i - 1].y ? 0 : count[i].y - count[i - 1].y
+        });
     }
-    else {
-        newConfirmed.push({x: count[count.length-1].x, y: 0});
-        
-        for (var i=count.length-2; i >= 0; i--) {
-            // If next day is less than previous day, use previous day
-            newConfirmed.push({x: count[i].x, y: count[i].y < count[i+1].y ? 0 : count[i].y - count[i+1].y});
-        }
-    }
-    
+
     return newConfirmed;
 }
 
 // Calculates moving average for specified number of days
 function calcMovingAverage(count, days) {
-    
-    // Check if ascending or descending order!
-    // Assumes dates are not ordered randomly, and no missing dates!
-    let ascending = true;
+
+    // Check if descending order. Assumes dates are not ordered randomly, and no missing dates!
     if (count.length > 1 && count[0].x > count[1].x) {
-        ascending = false;
+        count.reverse();
     }
-    
+
     let movingAvg = [];
-    for (var i=0; i<count.length; i++) {
-        var avg = 0;
-        for (var offset=0; offset<days; offset++) {
-          
-            if (ascending) {
-                avg += i-offset < 0 ? 0 : count[i-offset].y;
-            }
-            else {
-                avg += i+offset >= count.length ? 0 : count[i+offset].y;
-            }
+    for (let i = 0; i < count.length; i++) {
+        let avg = 0;
+        for (let offset = 0; offset < days; offset++) {
+            avg += i - offset < 0 ? 0 : count[i - offset].y;
         }
         avg /= days;
-        //movingAvg.push({x: count[i].x, y: Math.round(avg)});
-        movingAvg.push({x: count[i].x, y: avg});
+        movingAvg.push({
+            x: count[i].x,
+            y: avg
+        });
     }
 
     return movingAvg;
@@ -83,36 +70,24 @@ function calcMovingAverage(count, days) {
 
 // Calculates exponential moving average for specified number of days
 function calcExpMovingAverage(count, days) {
-    
-    // Check if ascending or descending order!
-    // Assumes dates are not ordered randomly, and no missing dates!
-    let ascending = true;
-    if (count.length > 1 && count[0].x > count[1].x) {
-        ascending = false;
-    }
-    
-    let movingAvg = [];
 
-    // Save prev EMA to keep precision for subsequent calcs
+    // Check if descending order. Assumes dates are not ordered randomly, and no missing dates!
+    if (count.length > 1 && count[0].x > count[1].x) {
+        count.reverse();
+    }
+
+    let movingAvg = [];
     let prevEma = 0;
-    let mult = 2/(days+1);
-    
-    if (ascending) {
-        for (var i=0; i<count.length; i++) {        
-            let ema = ((count[i].y - prevEma) * mult) + prevEma;
-            //movingAvg.push({x: count[i].x, y: Math.round(ema)});
-            movingAvg.push({x: count[i].x, y: ema});
-           prevEma = ema;
-        }
-    }
-    else {
-        for (var i=count.length-1; i>=0; i--) {
-            let ema = ((count[i].y - prevEma) * mult) + prevEma;
-            //movingAvg.push({x: count[i].x, y: Math.round(ema)});
-            movingAvg.push({x: count[i].x, y: ema});
-            prevEma = ema;
-        }
-    }
+    let mult = 2 / (days + 1);
+
+    count.forEach(row => {
+        let ema = ((row.y - prevEma) * mult) + prevEma;
+        movingAvg.push({
+            x: row.x,
+            y: ema
+        });
+        prevEma = ema;
+    })
 
     return movingAvg;
 }
@@ -120,44 +95,43 @@ function calcExpMovingAverage(count, days) {
 // Create the chart
 function createChart(series, divName, chartName) {
     JSC.Chart(divName, {
-        title: { 
-            position: 'center', 
-            label: { 
-                text: chartName, 
-                style_fontSize: 25 
-            } 
-        }, 
-        defaultPoint: { 
+        title: {
+            position: 'center',
+            label: {
+                text: chartName,
+                style_fontSize: 25
+            }
+        },
+        defaultPoint: {
             marker_visible: false,
             tooltip: '<span style="width:75px">%seriesName</span> %icon {%yvalue:n0}'
         },
-        yAxis: [ 
-                { id: 'mainY', formatString: 'n0' }, 
-                { 
-                  id: 'secondY', 
-                  scale_syncWith: 'mainY', 
-                  orientation: 'opposite', 
-                  line_color: '#e2e2e2', 
-                  defaultTick: { 
-                    enabled: false, 
-                    gridLine_visible: false
-                  } 
-                } 
-              ], 
+        yAxis: [{ id: 'mainY', formatString: 'n0' },
+        {
+            id: 'secondY',
+            scale_syncWith: 'mainY',
+            orientation: 'opposite',
+            line_color: '#e2e2e2',
+            defaultTick: {
+                enabled: false,
+                gridLine_visible: false
+            }
+        }
+        ],
         xAxis: {
             scale_type: "time",
             defaultTick_enabled: true,
-            crosshair_enabled: true, 
+            crosshair_enabled: true,
             formatString: 'MMM d',
             scale: {
-              interval: {
-                unit: "day",
-                multiplier: 7
-              },
+                interval: {
+                    unit: "day",
+                    multiplier: 7
+                },
             }
         },
-        legend: { 
-            position: 'inside top', 
+        legend: {
+            position: 'inside top',
             template: '%icon %name'
         },
         series: series
--- scripts/dataReader.js
+++ scripts/dataReader.js
@@ -1,100 +1,107 @@
 // Ignore dates before this date
-const minDate = new Date('2020-03-15');
-
-// Fetch csv from url, then convert cvs to series, then create chart
-function createChartFromGit(countyName, chartName, avgType) {
-    JSC.fetch('https://raw.githubusercontent.com/datadesk/california-coronavirus-data/master/latimes-county-totals.csv')
-    .then(function (response) {
-        return response.text();
-    })
-    .then(function (text) {
-        let dataAsJson = JSC.csv2Json(text);
-
-        let count = [];
-        dataAsJson.forEach(function (row) {
-            
-            // Filter only for specified county name
-            if (row.county == countyName) {
-                if (minDate.getTime() < stringToDate(row.date).getTime()) {
-
-                    let newCases = row.new_confirmed_cases;
-                    count.push({x: stringToDate(row.date), y: newCases == null || newCases == undefined ? 0 : parseInt(newCases, 10)});
-                }
-            }
-        });
-        calcAveragesAndChart(count, "chartDiv", chartName, avgType);
-    })
-    .catch(function (error) {
-        console.log(error);
-    });    
-}
-
-// Fetch csv from url, then convert csv to series, then create chart, given list of cities
-function createChartForCities(cities, chartDiv, avgType) {
-    JSC.fetch('https://raw.githubusercontent.com/datadesk/california-coronavirus-data/master/latimes-place-totals.csv')
-    .then(function (response) {
-        return response.text();    
-    })
-    .then(function (text) {
-        let dataAsJson = JSC.csv2Json(text);
-        for (let i=0; i<cities.length; i++) {
-            createChartForCity(dataAsJson, cities[i], chartDiv+cities[i], avgType);
-        }
-    })
-    .catch(function (error) {
-    	console.log(error);
-	});    
-}
-
-// Given json data, create series for each city, create chart
-function createChartForCity(dataAsJson, cityName, chartDiv, avgType) {
-     let count = [];
-     dataAsJson.forEach(function (row) {
-         
-         // Filter only for specified county name
-         if (row.place == cityName) {
-             if (minDate.getTime() < stringToDate(row.date).getTime()) {
-
-                 let cases = row.confirmed_cases;
-                 count.push({x: stringToDate(row.date), y: cases == null || cases == undefined ? 0 : parseInt(cases, 10)});
-             }
-         }
-     });
-     
-     let newCount = calcNewConfirmed(count);
-     
-     calcAveragesAndChart(newCount, chartDiv, cityName + " New Cases by Day", avgType);
-}
-
-// Read page source, convert to json, create chart
-// Data only available for Los Angeles County
-function createChartFromPageSource(chartName, url, avgType) {
-    $.ajax({
-        url: url,
-        type: 'GET',
-        success: function(data) {
-            var dom = $(data); 
-            
-            let count = [];
-            dom.filter('script').each(function() {
-                if (this.id == 'timeseries') {
-                    let json = $.parseJSON(this.innerHTML)
-                    for (let i=0; i<json.length; i++) {
-                        if (minDate.getTime() < stringToDate(json[i].date).getTime()) {
-                            if (json[i].agencies_count == json[i].agencies_updated || json[i].in_progress == false) {
-                                count.push({x: stringToDate(json[i].date), y: json[i].new_confirmed_cases});
-                            }
-                        }
-                    }
-                }
-            });
-            calcAveragesAndChart(count, "chartDiv", chartName, avgType);
-        }
-    });
-}
+const minDate = new Date('2020-03-15').getTime();
+var json;
 
 //Convert string to date, taking into account time zone diff
 function stringToDate(millis) {
     let date = new Date(millis);
     return new Date(date.getTime() + date.getTimezoneOffset() * 60000);
+}
+
+// Fetch csv from url, convert cvs to json, then create chart
+function createChartFromGit(countyName, chartName, avgType) {
+    if (json != null) {
+        calcAveragesAndChart(json, "chartDiv", chartName, avgType);
+        return;
+    }
+
+    $.ajax({
+        url: 'https://raw.githubusercontent.com/datadesk/california-coronavirus-data/master/latimes-county-totals.csv',
+        type: 'GET',
+        success: function (data) {
+            let count = [];
+            // Filter only for specified county name and min date
+            json = JSC.csv2Json(data)
+                .filter(row => row.county == countyName && minDate < stringToDate(row.date).getTime());
+
+            json.forEach(row => {
+                    let newCases = row.new_confirmed_cases;
+                    count.push({ 
+                        x: stringToDate(row.date), 
+                        y: newCases == null || newCases == undefined ? 0 : parseInt(newCases, 10) });
+                });
+
+            json = count;
+            calcAveragesAndChart(count, "chartDiv", chartName, avgType);
+        }
+    })
+}
+
+// Fetch csv from url, convert to json, loop to create chart for each city
+function createChartForCities(cities, chartDiv, avgType) {
+    if (json != null) {
+        cities.forEach(city => createChartForCity(json, city, chartDiv + city, avgType));
+        return;
+    }
+
+    $.ajax({
+        url: 'https://raw.githubusercontent.com/datadesk/california-coronavirus-data/master/latimes-place-totals.csv',
+        type: 'GET',
+        success: function (data) {
+            // convert to Json and filter only for min date
+            json = JSC.csv2Json(data)
+                .filter(row => minDate < stringToDate(row.date).getTime());
+            
+            cities.forEach(city => createChartForCity(json, city, chartDiv + city, avgType));
+        }
+    });
+}
+
+// Given json data of all cities, create json for each city, create chart
+function createChartForCity(dataAsJson, cityName, chartDiv, avgType) {
+    let count = [];
+    dataAsJson
+        .filter(row => row.place == cityName)
+        .forEach(row => {
+            let cases = row.confirmed_cases;
+            count.push({
+                x: stringToDate(row.date),
+                y: cases == null || cases == undefined ? 0 : parseInt(cases, 10)
+            });
+        });
+
+    let newCount = calcNewConfirmed(count);
+
+    calcAveragesAndChart(newCount, chartDiv, cityName + " New Cases by Day", avgType);
+}
+
+// Read page source, convert to json, create chart
+// Data only available for Los Angeles County
+function createChartFromPageSource(chartName, url, avgType) {
+    if (json != null) {
+        calcAveragesAndChart(json, "chartDiv", chartName, avgType);
+        return;
+    }
+
+    $.ajax({
+        url: url,
+        type: 'GET',
+        success: function (data) {
+            let count = [];
+            $(data).filter('script').each(function () {
+                if (this.id == 'timeseries') {
+                    $.parseJSON(this.innerHTML)
+                        .filter(row => minDate < stringToDate(row.date)
+                            && (row.agencies_count == row.agencies_updated || row.in_progress == false))
+                        .forEach(row => count.push({
+                            x: stringToDate(row.date),
+                            y: row.new_confirmed_cases
+                        }));
+                }
+            });
+
+            json = count;
+            calcAveragesAndChart(count, "chartDiv", chartName, avgType);
+        }
+    });
 }
